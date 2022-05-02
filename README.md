e90cc30d4a31fff2e38a14f501d34f5db2a135f2
mrparkersson <amoparkerprince78@gmail.com>
2022-05-02 21:50:42 +0000

    Make changes in technologies file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ## Personal Portfolio
+
+![Portfolio Website](../../assets/shot.png)
--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,51 @@
 import React from 'react';
 import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from './TechnologiesStyles';
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+const Technologies = () => (
+  <Section id="tech">
+    <SectionDivider />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've worked with both small and large firms, and I've built full-stack
+      apps for them using a variety of technologies and languages.
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem" />
+        <ListContainer>Front-end</ListContainer>
+        <ListParagraph>
+          Experience with <br /> React.js, HTML/CSS, Styled Components, Scss
+        </ListParagraph>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>Back-end</ListContainer>
+        <ListParagraph>
+          Experience with <br /> Node.js and Databases
+        </ListParagraph>
+      </ListItem>
+      <ListItem>
+        <DiZend size="3rem" />
+        <ListContainer>UI/UX</ListContainer>
+        <ListParagraph>
+          Experience with <br /> tools like Figma
+        </ListParagraph>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
